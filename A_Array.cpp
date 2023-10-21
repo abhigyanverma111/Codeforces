9b704fdb02f709a21833cb4b7490d6a57acc7bdd
Abhigyan Verma <abhigyanverma111@gmail.com>
2023-10-21 23:51:28 +0530

    does not have array declaration to save space

--- A_Array.cpp
+++ A_Array.cpp
@@ -5,27 +5,23 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    int temp;
     vector<int> v1, v2, v3;
     for(int i=0;i<n;i++){
-        if(arr[i] > 0) v2.push_back(arr[i]);
-        else if(arr[i] < 0) v1.push_back(arr[i]);
-        else{v3.push_back(arr[i]);}
-
-        
+        cin>>temp;
+        if(temp > 0) v2.push_back(temp);
+        else if(temp < 0) v1.push_back(temp);
+        else{v3.push_back(temp);}
     }
 
     if(v1.size() % 2 == 0){
-        int temp = v1[v1.size() - 1];
+        temp = v1[v1.size() - 1];
         v1.pop_back();
         v3.push_back(temp);
     }
 
     if(v2.size() == 0){
-        int temp = v1[v1.size() - 1];
+        temp = v1[v1.size() - 1];
         v1.pop_back();
         v2.push_back(temp);
 
